f20e50abd18afc180183c7020abcb6d36be69964
Jean Prunneaux <jean@prunneaux.com>
2022-07-22 22:00:04 +0200

    Fix JSON import and disable module for now

--- inc/import/global/json.php
+++ inc/import/global/json.php
@@ -7,14 +7,9 @@ abstract class JsonImport extends GlobalImport
 {
     protected $json;
 
-    /**
-     * Class constructor
-     * @param string $file    The path (or URL) to the JSON file to import
-     * @param array  $options Import options
-     */
-    public function __construct($file, $options=[])
+    public function load()
     {
-        $file = Storage::path($file);
+        $file = Storage::path($this->source);
 
         if (!preg_match('/^https*:\/\//', $file) && !file_exists($file)) {
             Logger::error("Import JSON: file ".$file." does not exists!");
@@ -24,7 +19,5 @@ abstract class JsonImport extends GlobalImport
 
         $content = file_get_contents($file);
         $this->json = json_decode($content);
-
-        parent::__construct($file, $options);
     }
 }
--- inc/import/json/import.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once('inc/import/global/json.php');
-
-class Import extends JsonImport
-{}
