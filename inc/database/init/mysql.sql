8fc94fcc84ded786d25161fb0f6fb433be770b0e
Jean Prunneaux <jean@prunneaux.com>
2024-02-03 12:37:02 +0100

    Update SQL init file

--- inc/database/init/mysql.sql
+++ inc/database/init/mysql.sql
@@ -9,12 +9,13 @@ CREATE TABLE IF NOT EXISTS `collection` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `cover` varchar(255) DEFAULT NULL,
   `owner` int(11) NOT NULL,
+  `type` varchar(255) DEFAULT '',
   `template` tinyint(4) NOT NULL DEFAULT 0,
   `visibility` int(10) NOT NULL DEFAULT 3,
-  `created` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 CREATE TABLE IF NOT EXISTS `collectionLabel` (
@@ -25,14 +26,14 @@ CREATE TABLE IF NOT EXISTS `collectionLabel` (
   UNIQUE KEY `collection_lang` (`collectionId`,`lang`),
   KEY `collection` (`collectionId`),
   CONSTRAINT `collectionLabel_ibfk_1` FOREIGN KEY (`collectionId`) REFERENCES `collection` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 CREATE TABLE IF NOT EXISTS `config` (
   `param` varchar(255) NOT NULL,
   `value` text DEFAULT NULL,
   PRIMARY KEY (`param`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 CREATE TABLE IF NOT EXISTS `item` (
@@ -40,12 +41,12 @@ CREATE TABLE IF NOT EXISTS `item` (
   `collectionId` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   `visibility` int(10) NOT NULL DEFAULT 0,
-  `created` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
   KEY `collection` (`collectionId`),
   CONSTRAINT `item_ibfk_1` FOREIGN KEY (`collectionId`) REFERENCES `collection` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 CREATE TABLE IF NOT EXISTS `itemProperty` (
@@ -58,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `itemProperty` (
   KEY `name` (`name`),
   CONSTRAINT `itemProperty_ibfk_1` FOREIGN KEY (`collectionId`) REFERENCES `collection` (`id`),
   CONSTRAINT `itemProperty_ibfk_2` FOREIGN KEY (`itemId`) REFERENCES `item` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 CREATE TABLE IF NOT EXISTS `loan` (
@@ -73,7 +74,7 @@ CREATE TABLE IF NOT EXISTS `loan` (
   PRIMARY KEY (`id`),
   KEY `itemId` (`itemId`),
   CONSTRAINT `loan_ibfk_1` FOREIGN KEY (`itemId`) REFERENCES `item` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 CREATE TABLE IF NOT EXISTS `notification` (
@@ -87,7 +88,7 @@ CREATE TABLE IF NOT EXISTS `notification` (
   KEY `type` (`type`),
   KEY `datetime` (`datetime`),
   CONSTRAINT `notification_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 CREATE TABLE IF NOT EXISTS `property` (
@@ -115,7 +116,7 @@ CREATE TABLE IF NOT EXISTS `property` (
   UNIQUE KEY `collection_name` (`collectionId`,`name`),
   KEY `name` (`name`),
   CONSTRAINT `property_ibfk_1` FOREIGN KEY (`collectionId`) REFERENCES `collection` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 CREATE TABLE IF NOT EXISTS `propertyLabel` (
@@ -128,7 +129,7 @@ CREATE TABLE IF NOT EXISTS `propertyLabel` (
   KEY `field` (`propertyId`),
   KEY `label` (`label`),
   CONSTRAINT `propertyLabel_ibfk_1` FOREIGN KEY (`propertyId`) REFERENCES `property` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 CREATE TABLE IF NOT EXISTS `token` (
@@ -142,7 +143,7 @@ CREATE TABLE IF NOT EXISTS `token` (
   KEY `user` (`userId`),
   KEY `type` (`type`),
   CONSTRAINT `token_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 CREATE TABLE IF NOT EXISTS `user` (
@@ -154,4 +155,4 @@ CREATE TABLE IF NOT EXISTS `user` (
   `avatar` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
